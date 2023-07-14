614f399af5372f93566cfa2f11d0fad836c657a5
vbandarovich <worldvb2000@gmail.com>
2023-07-14 22:40:09 +0300

    Added initial template with base nuget packages

--- SkinHuntApp/SkinHuntApp/Program.cs
+++ SkinHuntApp/SkinHuntApp/Program.cs
@@ -1,3 +1,9 @@
+using FluentValidation.AspNetCore;
+using Serilog;
+using SkinHunt.Application;
+using SkinHunt.Application.Common.Mapping;
+using SkinHunt.Infrastructure;
+
 namespace SkinHuntApp
 {
     public class Program
@@ -9,9 +15,30 @@ namespace SkinHuntApp
             // Add services to the container.
 
             builder.Services.AddControllers();
+
+            builder.Services.AddFluentValidationAutoValidation();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "SkinHuntApp API", Version = "v1" });
+            });
+
+            // Add services from DependencyInjection files
+            builder.Services.AddApplication(builder.Configuration);
+            builder.Services.AddInfrastructure(builder.Configuration);
+
+            builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+            {
+                builder.WithOrigins("*")
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+            }));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .CreateLogger();
 
             var app = builder.Build();
 
@@ -22,10 +49,16 @@ namespace SkinHuntApp
                 app.UseSwaggerUI();
             }
 
+            app.UseStatusCodePages();
+
+            app.UseRouting();
+
+            app.UseCors("corsapp");
+
             app.UseHttpsRedirection();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
