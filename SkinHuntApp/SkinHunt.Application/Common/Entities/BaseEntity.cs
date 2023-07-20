5c645767bbd0be0356792436d231cf9a5ebb6938
PLUMBIR <bondarovich.kirya@gmail.com>
2023-07-20 22:23:08 +0300

    Add db entities

--- SkinHuntApp/SkinHunt.Application/Common/Entities/BaseEntity.cs
+++ SkinHuntApp/SkinHunt.Application/Common/Entities/BaseEntity.cs
@@ -1,7 +1,12 @@
-﻿namespace SkinHunt.Application.Common.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkinHunt.Application.Common.Entities
 {
     public abstract class BaseEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
     }
 }
