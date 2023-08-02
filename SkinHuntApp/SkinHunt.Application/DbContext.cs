708a38be93fc479237050c251cf20d95c2d86fbc
PLUMBIR <bondarovich.kirya@gmail.com>
2023-08-02 22:38:13 +0300

    Updated ConnectionString to db, created InitialMigrations

--- SkinHuntApp/SkinHunt.Application/DbContext.cs
+++ SkinHuntApp/SkinHunt.Application/DbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SkinHunt.Application.Common.Entities;
 
 namespace SkinHunt.Application
 {
     public class DbContext : IdentityDbContext<IdentityUser>
     {
+        public DbSet<SkinEntity> Skins { get; set; }
+
         public DbContext(DbContextOptions<DbContext> options) : base(options)
         {
         }
