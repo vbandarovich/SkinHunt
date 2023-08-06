e3e0390d117743a3c107ccae8935a7a0737fc059
PLUMBIR <bondarovich.kirya@gmail.com>
2023-08-06 22:38:34 +0300

    Added Enums with gun types

---
+++ SkinHuntApp/SkinHunt.Application/Common/Enums/SkinsEnum.cs
@@ -0,0 +1,94 @@
+﻿namespace SkinHunt.Application.Common.Enums
+{
+    public enum Knifes
+    {
+        Bayonet,
+        Bowie,
+        Butterfly,
+        Falchion,
+        Flip,
+        Gut,
+        Huntsman,
+        Karambit,
+        M9_Bayonet,
+        Navaja,
+        Shadow_Daggers,
+        Stiletto,
+        Talon,
+        Ursus,
+        Classic,
+        Skeleton,
+        Nomad,
+        Survival,
+        Paracord
+    }
+
+    public enum Gloves
+    {
+        Bloodhound,
+        Driver,
+        Hand_Wraps,
+        Hydra,
+        Moto,
+        Specialist,
+        Sport
+    }
+
+    public enum Pistols
+    {
+        CZ75_Auto,
+        Desert_Eagle,
+        Dual_Berettas,
+        Five_SeveN,
+        Glock_18,
+        P2000,
+        P250,
+        R8_Revolver,
+        Tec_9,
+        USP_S
+    }
+
+    public enum SMGs
+    {
+        MAC_10,
+        MP5_SD,
+        MP7,
+        MP9,
+        P90,
+        PP_Bizon,
+        UMP_45
+    } 
+
+    public enum AssaultRifles
+    {
+        AK_47,
+        AUG,
+        FAMAS,
+        Galil_AR,
+        M4A1_S,
+        M4A4,
+        SG_553
+    }
+
+    public enum SniperRifles
+    {
+        AWP,
+        G3SG1,
+        SCAR_20,
+        SSG_08
+    }
+
+    public enum Shotguns
+    {
+        MAG_7,
+        Nova,
+        Sawed_Off,
+        XM1014
+    }
+
+    public enum MachineGuns
+    {
+        M249,
+        Negev
+    }
+}
