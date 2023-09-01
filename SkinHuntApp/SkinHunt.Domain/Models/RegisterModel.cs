62e47f7bd2038b8ae54e4df825858898c61a4a83
PLUMBIR <bondarovich.kirya@gmail.com>
2023-09-01 19:47:15 +0300

    Created RegisterModel and AuthorizationController

---
+++ SkinHuntApp/SkinHunt.Domain/Models/RegisterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SkinHunt.Domain.Models
+{
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        
+        public string Email { get; set; }  
+
+        public string Password { get; set; } 
+    }
+}
--- SkinHuntApp/SkinHuntApp/Controllers/AuthorizationController.cs
+++ SkinHuntApp/SkinHuntApp/Controllers/AuthorizationController.cs
@@ -1,14 +1,64 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using SkinHunt.Application.Commands;
+using SkinHunt.Domain.Models;
 
 namespace SkinHunt.Service.Controllers
 {
-    [Route("/authorization/")]
+    [Route("api/authorization")]
     [ApiController]
-    public class AuthorizationController : AppControllerBase
+    public class SignUpController : AppControllerBase
     {
-        public AuthorizationController()
-        {              
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<InitRolesCommandHandler> _logger;
+
+        public SignUpController(UserManager<IdentityUser> userManager, IConfiguration configuration, ILogger<InitRolesCommandHandler> logger)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("createUser")]
+        public async Task<object> Post(RegisterModel registerModel)
+        {
+            try
+            {
+                var user = new IdentityUser
+                {
+                    UserName = registerModel.Name,
+                    Email = registerModel.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, registerModel.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, "user");
+
+                    _logger.LogInformation("User has been created");
+
+                    return Ok(new
+                    {
+                        id = user.Id,
+                        userName = user.UserName,
+                        email = user.Email,
+                    });
+                }
+                return Task.FromResult(false);
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"SignInAsync was fail with exception: {ex.Message}");
+
+                return null;
+            }
+            
         }
 
         [AllowAnonymous]
