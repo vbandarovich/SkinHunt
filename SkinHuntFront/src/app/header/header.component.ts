f536fd75b32e01a2b05bdbd3f3bb44ac6cce598a
vbandarovich <worldvb2000@gmail.com>
2024-06-05 23:52:13 +0300

    Fixes dropdowns

--- SkinHuntFront/src/app/header/header.component.ts
+++ SkinHuntFront/src/app/header/header.component.ts
@@ -1,38 +1,33 @@
-import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
-import { MdbRippleModule } from 'mdb-angular-ui-kit/ripple';
-import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import { MdbDropdownDirective } from 'mdb-angular-ui-kit/dropdown';
-import { CommonModule } from '@angular/common';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { MdbModalModule, MdbModalRef, MdbModalService } from 'mdb-angular-ui-kit/modal';
+import {MdbCollapseModule} from 'mdb-angular-ui-kit/collapse';
+import {MdbRippleModule} from 'mdb-angular-ui-kit/ripple';
+import {MdbDropdownDirective, MdbDropdownModule} from 'mdb-angular-ui-kit/dropdown';
+import {ChangeDetectionStrategy, Component, signal, ViewChild} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {MdbModalModule, MdbModalRef, MdbModalService} from 'mdb-angular-ui-kit/modal';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [    
-    CommonModule,  
+  imports: [
+    CommonModule,
     MdbCollapseModule,
     MdbRippleModule,
     MdbDropdownModule,
     MdbModalModule
   ],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 
-export class HeaderComponent implements AfterViewInit {
-  @ViewChild('dropdown') dropdown!: MdbDropdownDirective;
+export class HeaderComponent {
+  @ViewChild('flagDropdown') flagDropdown!: MdbDropdownDirective;
+  @ViewChild('currencyDropdown') currencyDropdown!: MdbDropdownDirective;
 
   signInModalRef: MdbModalRef<SignInComponent> | null = null;
 
   constructor(private modalService: MdbModalService) {
-  }
-
-  ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.dropdown.show();
-    }, 2000);
   }
 
   openSignInModal() {
